0705314812dcae510617137017cce6afa0aec5d8
Nick Frichette <frichetten@gmail.com>
2017-07-11 01:42:15 -0500

    Implemented FireTraffic on the index

---
+++ js/FireTraffic-DB.js
@@ -0,0 +1,19 @@
+function writeUserData(ip, city, country, time) {
+          path = String(window.location.pathname);
+          firebase.database().ref('/'+path.substring(0,path.indexOf("."))+'/'+String(new Date())).set({
+          ip: ip,
+          city: city,
+          country: country,
+          time: time
+        });
+      };
+
+      $.ajax({
+        url:        'http://ip-api.com/json?callback=?',
+        dataType:   "json", 
+        success:    function(data){
+          info = JSON.stringify(data, null, 2);	
+          d = JSON.parse(info);
+          writeUserData(d['query'],d['city'],d['country'],String(new Date()));
+        }
+      });
