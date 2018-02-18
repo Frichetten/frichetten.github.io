b9301791e60201d12a67078b8bc3c9e186f8715a
Nick Frichette <frichetten@gmail.com>
2018-02-18 14:05:29 -0600

    Removed outdated files from previous versions (finally cleaning house)

--- js/fun.js
+++
@@ -1,9 +0,0 @@
-!function(){
-	try{
-		var message=["\t=================================================\n\t=   This is not the code you're looking for :)  =\n\t=================================================\n"].join("").split(""),i=message.length,output=[],colors=[];
-		for(message.reverse();i--;)
-			output.push("%s"+message[i]),
-			""===message[i]?colors.push("color: red"):colors.push("")
-			;console.log.apply(console,[output.join("")].concat(colors))
-	}catch(err){}
-}();
