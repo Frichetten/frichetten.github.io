317726083309d0e48ad2a6781d3fe8e32fcc85cb
Nick Frichette <nick@frichetten.com>
2018-09-22 14:38:35 -0500

    Fixed the server.ts to be more accurate

--- server.ts
+++ server.ts
@@ -5,19 +5,28 @@ import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as secrets from './config/secrets';
+import * as Article from './models/article';
+import * as fs from 'fs';
+
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-import { join } from 'path';
+mongoose.connect(secrets.database, {useNewUrlParser:true});
+mongoose.connection.on('connected', () => {
+  console.log("Connected to database " + secrets.database);
+});
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
-const APP_NAME = 'frichetten-com';
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: ' + err);
+  process.exit();
+});
 
 const {
   AppServerModuleNgFactory,
   LAZY_MODULE_MAP
-} = require(`./dist/${APP_NAME}-server/main`);
+} = require(`./dist/frichetten-com-server/main`);
 
 enableProdMode();
 
@@ -33,26 +42,43 @@ app.engine(
 );
 
 app.set('view engine', 'html');
-
-app.get('/**/*', (req, res) => {
-  res.render(join(DIST_FOLDER, APP_NAME, 'index'), {
-    req,
-    res
-  });
-});
-
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, APP_NAME));
+app.set('views', './dist');
 
 // Static Assets
-app.get('*.*', express.static(join(DIST_FOLDER, APP_NAME)));
+app.get('*.*', express.static('./dist'));
 
 // Point all routes to Universal
 app.get('*', (req, res) => {
   res.render('index', { req });
 });
 
+app.post('/torbandwidth', (req, res) => {
+  // Here we need to read the file
+  fs.readFile('/home/nick/bandwidth','utf8', function(err, contents) {
+    res.send({"data" : contents});
+  });
+});
+
+// The blog endpoints
+app.post('/blog', (req, res) => {
+  Article.getAllArticleThumbs( (err, success) => {
+    if(success){
+      res.send(success);
+    }
+  });
+});
+
+app.post('/blog/*', (req, res) => {
+  var a = req.originalUrl;
+  a = a.substring(a.lastIndexOf("/")+1);
+  Article.getArticle(a, (err, success) => {
+    if(success){
+      res.send(success);
+    }
+  })
+});
+
 // Start Express Server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
+app.listen(4200, () => {
+  console.log(`Node Express server listening on http://localhost:4200`);
 });
