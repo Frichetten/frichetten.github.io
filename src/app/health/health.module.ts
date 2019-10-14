c79a0643d5eb268596969b03920a3b20740d3ad6
Nick Frichette <nick@frichetten.com>
2019-10-14 10:16:39 -0500

    Implemented a health endpoint

---
+++ src/app/health/health.module.ts
@@ -0,0 +1,22 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-health',
+  templateUrl: './health.component.html',
+})
+export class HealthComponent {
+  constructor() {}
+}
+
+@NgModule({
+  declarations: [HealthComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: HealthComponent }
+    ])
+  ],
+})
+export class HealthModule {}
