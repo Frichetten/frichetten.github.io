8abb7e2796078568c331ccdb3768466b41c46b40
Nick Frichette <nick@frichetten.com>
2020-03-26 21:49:56 -0500

    Fixed mixed content loading in matomo

--- themes/hacker/static/js/matomo.js
+++ themes/hacker/static/js/matomo.js
@@ -3,7 +3,7 @@ var _paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//matomo.frichetten.com/";
+  var u="https://matomo.frichetten.com/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
